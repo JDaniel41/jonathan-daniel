e57d88b93d2d41d96b1ebefe236d042b5969f06c
JOnathan Daniel <gcagrad19@gmail.com>
2021-10-22 00:30:10 -0400

    Added Starter Components

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -7,8 +7,10 @@ function TopNavBar() {
                 <Navbar.Brand href="#home">Jonathan Daniel</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#experiences">Experiences</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
