1af256e98728ff55f4e4e8606dad3b2c4a6961fd
JOnathan Daniel <gcagrad19@gmail.com>
2021-11-03 00:59:48 -0400

    Added Meta Logo

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -4,7 +4,7 @@ import holderjs from "holderjs";
 export default function ExperienceCard({ cardTitle, cardText }) {
     return (
         <Card className="m-5">
-            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Img variant="top" src={require("../assets/metaLogo.png")} />
             <Card.Body>
                 <Card.Title>{cardTitle}</Card.Title>
                 <Card.Text>{cardText}</Card.Text>
