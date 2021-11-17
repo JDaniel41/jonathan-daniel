008ab25e13fb6b7171e3c0721314e3b41c2d6c92
JOnathan Daniel <gcagrad19@gmail.com>
2021-11-17 18:47:43 -0500

    Finalish Layout Edits

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,48 @@
 import { Stack, Image } from "react-bootstrap";
 import envelope from "../img/envelope.svg";
 
-export default function Projects() {
+export default function Contact() {
     return (
         <Stack className="bg-dark">
             <h1 className="text-white text-center">Contact Me</h1>
+            <h2 className="text-white text-center">I would love to connect!</h2>
 
-            <Stack direction="vertical">
-                <h1 className="text-white text-center">Email</h1>
-                <Image
-                    src={envelope}
-                    style={{ height: "100px", width: "100px" }}
-                ></Image>
+            <Stack direction="horizontal">
+                <Stack
+                    direction="vertical"
+                    className=" d-flex align-items-center"
+                >
+                    <h1 className="text-white text-center">Email</h1>
+                    <Image
+                        src={envelope}
+                        style={{ height: "100px", width: "100px" }}
+                    ></Image>
+                    <p className="text-white">jmdanie@clemson.edu</p>
+                </Stack>
+
+                <Stack
+                    direction="vertical"
+                    className=" d-flex align-items-center"
+                >
+                    <h1 className="text-white text-center">LinkedIn</h1>
+                    <Image
+                        src={envelope}
+                        style={{ height: "100px", width: "100px" }}
+                    ></Image>
+                    <p className="text-white">jmdanie@clemson.edu</p>
+                </Stack>
+
+                <Stack
+                    direction="vertical"
+                    className=" d-flex align-items-center"
+                >
+                    <h1 className="text-white text-center">GitHub</h1>
+                    <Image
+                        src={envelope}
+                        style={{ height: "100px", width: "100px" }}
+                    ></Image>
+                    <p className="text-white">jmdanie@clemson.edu</p>
+                </Stack>
             </Stack>
         </Stack>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-    return <div></div>;
+    return (
+        <div className="bg-dark m-0">
+            <p className="text-white">
+                This was designed and developed by Jonathan Daniel.
+            </p>
+        </div>
+    );
 }
