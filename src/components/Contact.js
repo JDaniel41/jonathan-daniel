9f47ce6722a50dc7036fbe03ae39e6e15b84404b
Jonathan Daniel <gcagrad19@gmail.com>
2021-11-17 18:31:57 -0500

    T

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,18 @@
-import { Stack } from "react-bootstrap";
+import { Stack, Image } from "react-bootstrap";
+import envelope from "../img/envelope.svg";
 
 export default function Projects() {
     return (
         <Stack className="bg-dark">
             <h1 className="text-white text-center">Contact Me</h1>
+
+            <Stack direction="vertical">
+                <h1 className="text-white text-center">Email</h1>
+                <Image
+                    src={envelope}
+                    style={{ height: "100px", width: "100px" }}
+                ></Image>
+            </Stack>
         </Stack>
     );
 }
