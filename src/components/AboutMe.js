381f2876ec80aad89da49118481645bc2f6593af
JOnathan Daniel <gcagrad19@gmail.com>
2021-11-18 11:01:29 -0500

    Moved Button

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,13 +4,13 @@ import resume from "../assets/JonathanDanielResume.pdf";
 
 export default function AboutMe() {
     return (
-        <Stack className="d-flex align-items-center" id="about">
+        <Stack className="d-flex align-items-center my-5" id="about">
             <h1>About Me</h1>
 
             <Stack
                 direction="horizontal"
                 gap={3}
-                className="d-flex justify-content-center align-items-start m-5"
+                className="d-flex justify-content-center align-items-start mx-5"
             >
                 <Image
                     src={headshot}
@@ -26,7 +26,9 @@ export default function AboutMe() {
                         Hello! My name is <b>Jonathan</b> and I am a third-year
                         Computer Science student at Clemson University. My main
                         interests are in backend web development and data
-                        science.
+                        science. In the past, I have interned for State Farm and
+                        Meta Platforms, Inc., and will be interning for Lyft in
+                        Summer 2022.
                         <br /> <br />
                         Currently, I conduct ML research through the
                         Watson-In-The-Watt Creative Inquiry program. My
@@ -34,16 +36,17 @@ export default function AboutMe() {
                         allowed me to work with a variety of ML libraries and
                         frameworks such as TensorFlow, PyTorch, and
                         Scikit-Learn.
-                        <br />
+                        <br /> <br />I am also a member of the Clemson
+                        University National Scholars Program and am part of
+                        their 10-student 2019 cohort.
                     </p>
-
-                    <div className="d-flex justify-content-center">
-                        <Button variant="primary" href={resume}>
-                            Download my Resume
-                        </Button>
-                    </div>
                 </Stack>
             </Stack>
+            <div className="d-flex justify-content-center">
+                <Button variant="primary" href={resume}>
+                    Download my Resume
+                </Button>
+            </div>
         </Stack>
     );
 }
