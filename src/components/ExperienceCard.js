06bb69f83e7140a57209b061cd7056fe10238356
Jonathan Daniel <gcagrad19@gmail.com>
2021-11-26 21:49:07 -0500

    Fixed Cards for Mobile

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -9,7 +9,7 @@ export default function ExperienceCard({
 }) {
     return (
         <Col className="d-flex justify-content-center my-2">
-            <Card style={{ width: "500px", minWidth: "0px" }}>
+            <Card style={{ width: "400px", minWidth: "0px" }}>
                 <div
                     style={{ height: "30%", width: "100%" }}
                     className="d-flex text-center align-items-center justify-content-center"
