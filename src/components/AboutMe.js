fe2b06343a7a84e34831d7c20d54623169778b6c
Jonathan Daniel <gcagrad19@gmail.com>
2022-07-26 20:24:17 -0700

    Updates

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -32,18 +32,19 @@ export default function AboutMe() {
                             University. My main interests are in backend web
                             development and data science. In the past, I have
                             interned for Meta, State Farm and the Clemson Energy
-                            Visualization and Analytics Center (CEVAC). This
-                            Summer, I will be working as a Software Engineering
-                            Intern for Lyft.
+                            Visualization and Analytics Center (CEVAC). Most
+                            recently, I completed a Softawre Engineering
+                            Internship at Lyft.
                             <br /> <br />
                             Currently, I conduct ML research through the
                             Watson-In-The-Watt Creative Inquiry program. My
                             experiences with the Creative Inquiry program have
                             allowed me to work with a variety of ML libraries
                             and frameworks such as TensorFlow, PyTorch, and
-                            Scikit-Learn. In addition to the CI, I also conduct
-                            research on Occupancy Modeling and Power Forecasting
-                            through the program's collaboration with CEVAC.
+                            Scikit-Learn. In addition to the CI, I also
+                            conducted research on Occupancy Modeling and Power
+                            Forecasting through the program's collaboration with
+                            CEVAC.
                             <br /> <br />I am also a member of the Clemson
                             University National Scholars Program and am part of
                             their 10-student 2019 cohort.
